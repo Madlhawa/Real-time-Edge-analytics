c8b1280d857d7941a098dd346393733c18e004b7
Madlhawa <madhawa242@gmail.com>
2019-05-27 12:31:20 +0530

    test

---
+++ real1.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+
+import smbus
+import time
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+import paho.mqtt.publish as publish
+import datetime
+server = "broker.hivemq.com"
+
+# Define some device parameters
+I2C_ADDR  = 0x3f # I2C device address
+LCD_WIDTH = 16   # Maximum characters per line
+
+# Define some device constants
+LCD_CHR = 1 # Mode - Sending data
+LCD_CMD = 0 # Mode - Sending command
+
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
+LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
+
+LCD_BACKLIGHT  = 0x08  # On
+#LCD_BACKLIGHT = 0x00  # Off
+
+ENABLE = 0b00000100 # Enable bit
+
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+#Open I2C interface
+#bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
+bus = smbus.SMBus(1) # Rev 2 Pi uses 1
+
+pin = 13
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin,GPIO.IN)
+
+GPIO.setmode(GPIO.BOARD)
+PIN_TRIGGER = 7
+PIN_ECHO = 11
+GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+GPIO.setup(PIN_ECHO, GPIO.IN)
+GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+sensor = Adafruit_DHT.DHT11
+
+# Example using a Beaglebone Black with DHT sensor
+# connected to pin P8_11.
+pin = '27'
+
+def dis():
+    print("Waiting for sensor to settle")
+    time.sleep(0.05)
+    print("Calculating distance")
+    GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+    time.sleep(0.00001)
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+    
+    while GPIO.input(PIN_ECHO)==0:
+        pulse_start_time = time.time()
+    while GPIO.input(PIN_ECHO)==1:
+        pulse_end_time = time.time()
+    
+    time.sleep(0.5)
+    pulse_duration = pulse_end_time - pulse_start_time
+    distance = round(pulse_duration * 17150, 2)
+    print("Distance: "+str(distance)+"cm")
+    return(distance)
+
+
+def lcd_init():
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off 
+  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+  time.sleep(E_DELAY)
+
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = the data
+  # mode = 1 for data
+  #        0 for command
+
+  bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
+  bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
+
+  # High bits
+  bus.write_byte(I2C_ADDR, bits_high)
+  lcd_toggle_enable(bits_high)
+
+  # Low bits
+  bus.write_byte(I2C_ADDR, bits_low)
+  lcd_toggle_enable(bits_low)
+
+def lcd_toggle_enable(bits):
+  # Toggle enable
+  time.sleep(E_DELAY)
+  bus.write_byte(I2C_ADDR, (bits | ENABLE))
+  time.sleep(E_PULSE)
+  bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
+  time.sleep(E_DELAY)
+
+def lcd_string(message,line):
+  # Send string to display
+
+  message = message.ljust(LCD_WIDTH," ")
+
+  lcd_byte(line, LCD_CMD)
+
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]),LCD_CHR)
+
+def main():
+  # Main program block
+  now = datetime.datetime.now()
+  # Initialise display
+  lcd_init()
+  
+  while True:
+    print(str(now.year)+"-"+str(now.month)+"-"+str(now.day))
+    distance = dis()
+    publish.single("madhawa/dis", distance, hostname=server)
+    lcd_string("   Dis="+str(dis())+"cm",LCD_LINE_1)
+      
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    publish.single("madhawa/temp/realtime", temperature, hostname=server)
+    publish.single("madhawa/humid", humidity, hostname=server)
+    
+    if humidity is not None and temperature is not None:
+        publish.single("madhawa/temp/diff", str(float(temperature) - 27.6031), hostname=server)
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        print("Temp Diff = "+str(float(temperature) - 27.6031))
+        lcd_string("  T="+str(temperature)[:-2]+"'C"+"  H="+str(humidity)[:-2]+"%",LCD_LINE_2)
+    else:
+        print('Failed to get reading. Try again!')
+    
+if __name__ == '__main__':
+
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    lcd_byte(0x01, LCD_CMD)
+
